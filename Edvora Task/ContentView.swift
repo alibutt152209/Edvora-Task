4ad96a8b7ddfb8f33457aff13cfe0207c3faf65a
Ali Butt <alibutt152209@gmail.com>
2022-01-29 19:30:27 +0500

    Initial Commit

---
+++ Edvora Task/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Edvora Task
+//
+//  Created by Ali Butt on 29/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Edvora Task/Edvora_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Edvora_TaskApp.swift
+//  Edvora Task
+//
+//  Created by Ali Butt on 29/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Edvora_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
